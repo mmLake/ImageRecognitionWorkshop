5c1a1a24f53c794ff1e61731d513367aa3bab1b8
Maya Lake <abcdcookiemonster@gmail.com>
2018-04-15 07:02:47 -0700

    fixed setIcon

--- Client/Artworks/index.js
+++ Client/Artworks/index.js
@@ -5,10 +5,10 @@ const path = require('path')
 let win
 
 function createWindow() {
-   win = new BrowserWindow({width: 540, height: 900})
+   win = new BrowserWindow({width: 540, height: 900, icon:path.join(__dirname, 'assets/icons/png/64x64.png')})
 
    win.setMenu(null); // Remove default menu
-   win.setIcon(path.join(__dirname, 'assets/icons/png/64x64.png'));
+//    win.setIcon(path.join(__dirname, 'assets/icons/png/64x64.png'));
    win.loadURL(url.format({
       pathname: path.join(__dirname, 'index.html'),
       protocol: 'file:',
